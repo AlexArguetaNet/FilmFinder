a164aa9b97b64bfc791720e43df9b18e624ac210
Alex Argueta <alexarguetanet@gmail.com>
2025-03-18 22:03:22 -0400

    Implemented useEffect function to get popular movies on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        
+        const getPopularMovies = async () => {
+            try {
+
+                const response = await fetch("http://localhost:4001/popular-movies", {
+                    method: "GET"
+                });
+    
+                const movies = await response.json();
+    
+                setMovies(movies.data.results);
+    
+            } catch (err) {
+                alert("Error getting movies");
+            }
+        }
+
+        getPopularMovies();
+
+    }, []);
+
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='movie-grid'>
+            {movies.length > 0 && movies.map((elem, key) => (
+                <p key={key} >{ elem.original_title }</p>
+            ))}
+        </div>
+    </div>
   )
 }
