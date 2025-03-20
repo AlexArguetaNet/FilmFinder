a44d97ae38baf4ca4cda7de6ef22b5939249d2da
Alex Argueta <alexarguetanet@gmail.com>
2025-03-20 19:16:34 -0400

    Added more styling to the home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,12 @@ import { BiSolidMoviePlay } from "react-icons/bi";
 export default function Navbar() {
   return (
     <nav>
-        <div className='nav-bar-content'>
+        <div className='title'>
             <BiSolidMoviePlay />
             <p>Film Finder</p>
+        </div>
+        <div>
+            <button className='login-button'>Login</button>
         </div>
     </nav>
 )
